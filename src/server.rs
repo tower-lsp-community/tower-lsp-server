b671f7f4a47bf96eb83093f61c5bbcb8f395b751
Igor Aleksanov <popzxc@yandex.ru>
2026-09-09 10:46:41 +0400

    fix: Fix textDocument/codeAction response

--- src/server.rs
+++ src/server.rs
@@ -1038,7 +1038,7 @@ rpc! {
         ///   information. However it allows them to better group code action, for example, into
         ///   corresponding menus (e.g. all refactor code actions into a refactor menu).
         #[rpc(name = "textDocument/codeAction")]
-        async fn code_action(&self, params: CodeActionParams) -> Result<Option<CodeActionResponse>> {
+        async fn code_action(&self, params: CodeActionParams) -> Result<Option<Vec<CodeActionResponse>>> {
             let _ = params;
             error!("got a `textDocument/codeAction` request, but it is not implemented");
             Err(Error::method_not_found())
